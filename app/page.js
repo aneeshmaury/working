049ad8ffc4652cf967f429d22ce2c2a9c36eb7d3
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-08 08:33:22 +0530

    delet cofig file

--- app/page.js
+++ app/page.js
@@ -1,14 +1,39 @@
-import { IoMdArrowBack } from "react-icons/io";
-import { RxCross2 } from "react-icons/rx";
+"use client"
+import { Input } from "postcss";
+import React, { useState } from 'react';
+import { FaUserCircle } from "react-icons/fa";
+
 export default function Home() {
-  return (
-   <>
-    <nav className="flex justify-around mt-5 ">
-    <div className="arr text-2xl relative left-[-24px]"><IoMdArrowBack /></div>
-    <div className="midle flex items-center">24<RxCross2 />7 Help</div>
-   <div></div>
-    </nav>
-    <div className=" border-b h-1 border-zinc-500 mt-2"></div>
+  const [inputValue, setInputValue] = useState('');
+   const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  return <>
+    <div className="flex items-center mt-5 gap-3">
+    <div className="arr text-zinc-400 ml-2 text-3xl"><FaUserCircle /></div>
+    <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2"> Please elaborate your issue for faster resolution</div>
+ </div>
+ <div className="midl ">
+  <div>
+  
+<p className="bg-green-400 rounded-md float-right	mr-4 mt-6 text-sm" style={{ width: '250px', wordWrap: 'break-word', padding:"3px" }}>{inputValue}</p>
+</div>
+
+ <input
+        type="text"
+        value={inputValue}  
+        onChange={handleInputChange} 
+      />
+
+  <div className="lastr flex gap-4">
+<div className="arr text-zinc-400 ml-2 text-3xl"><FaUserCircle /></div>
+    <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2"> Thank you for providing the details. We will assign you to the agent and our agent will connect with you soon</div>
+
+  </div>
+
+
+ </div>
+  
    </>
-  );
+  
 }
