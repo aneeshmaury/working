039b9258d72a1524e9c1b0dbaf9ec79ed518b919
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-08 08:47:53 +0530

    jfhfj

--- app/page.js
+++ app/page.js
@@ -1,39 +1,43 @@
 "use client"
-import { Input } from "postcss";
 import React, { useState } from 'react';
 import { FaUserCircle } from "react-icons/fa";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
-   const handleInputChange = (event) => {
+  
+  const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
-  return <>
-    <div className="flex items-center mt-5 gap-3">
-    <div className="arr text-zinc-400 ml-2 text-3xl"><FaUserCircle /></div>
-    <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2"> Please elaborate your issue for faster resolution</div>
- </div>
- <div className="midl ">
-  <div>
   
-<p className="bg-green-400 rounded-md float-right	mr-4 mt-6 text-sm" style={{ width: '250px', wordWrap: 'break-word', padding:"3px" }}>{inputValue}</p>
-</div>
-
- <input
-        type="text"
-        value={inputValue}  
-        onChange={handleInputChange} 
-      />
-
-  <div className="lastr flex gap-4">
-<div className="arr text-zinc-400 ml-2 text-3xl"><FaUserCircle /></div>
-    <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2"> Thank you for providing the details. We will assign you to the agent and our agent will connect with you soon</div>
-
-  </div>
-
-
- </div>
-  
-   </>
-  
+  return (
+    <>
+      <div className="flex items-center mt-5 gap-3">
+        <div className="arr text-zinc-400 ml-2 text-3xl"><FaUserCircle /></div>
+        <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2">
+          Please elaborate your issue for faster resolution
+        </div>
+      </div>
+      
+      <div className="midl">
+        <div>
+          <p className="bg-green-400 rounded-md relative left-28 mr-4 mt-6 text-sm" style={{ width: '250px', wordWrap: 'break-word', padding:"9px" }}>
+            {inputValue}
+          </p>
+        </div>
+        
+        <div className="lastr flex gap-4 mt-6">
+          <div className="arr text-zinc-400 ml-2  mt-4 text-3xl"><FaUserCircle /></div>
+          <div className="midle flex text-sm items-center w-[250px] bg-slate-200 rounded-md p-2">
+            Thank you for providing the details. We will assign you to the agent and our agent will connect with you soon
+          </div>
+        </div>
+        
+        <input className="mt-11 border-2 border-black"
+          type="text"
+          value={inputValue}  
+          onChange={handleInputChange} 
+        />
+      </div>
+    </>
+  );
 }
