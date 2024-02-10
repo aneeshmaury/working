93587e5a88c19044f8015e5a57dd80ee462fa1ab
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-10 19:51:40 +0530

    jkdek

--- app/chat1.js
+++ app/chat1.js
@@ -11,17 +11,17 @@ const Chatmain = () => {
   return (
     <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
       <div className="mb-4">
-        <div className="bg-blue-100 w-[250px] p-2 rounded-lg inline-block">
+        <div className="bg-blue-100 w-[270px] p-2 pr-4 rounded-lg inline-block">
           <p className="text-[15px]"> Please elaborate your issue for faster resolution</p>
         </div>
       </div>
       <div className="mb-4 text-right">
-        <div className="bg-green-100 p-2 rounded-lg inline-block">
+        <div className="bg-green-100 p-3 rounded-lg inline-block">
           <p className="text-[15px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"9px" }} >{inputValue} </p>
         </div>
       </div>
       <div className="mb-4">
-        <div className="bg-blue-100 w-[250px] p-2 rounded-lg inline-block">
+        <div className="bg-blue-100 w-[270px] p-3 pr-5 rounded-lg inline-block">
           <p className="text-[15px]"> I understand how upsetting it's to loose your money.
 
 We have raised a ticket for your issue.
