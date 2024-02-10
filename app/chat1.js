b0e52085f05ba163c8dc901a33482860af971a23
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-10 19:47:32 +0530

    update

---
+++ app/chat1.js
@@ -0,0 +1,42 @@
+// components/Chat.js
+import React from 'react';
+import { useState } from 'react';
+
+const Chatmain = () => {
+    const [inputValue, setInputValue] = useState('');
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+      };
+
+  return (
+    <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
+      <div className="mb-4">
+        <div className="bg-blue-100 w-[250px] p-2 rounded-lg inline-block">
+          <p className="text-[15px]"> Please elaborate your issue for faster resolution</p>
+        </div>
+      </div>
+      <div className="mb-4 text-right">
+        <div className="bg-green-100 p-2 rounded-lg inline-block">
+          <p className="text-[15px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"9px" }} >{inputValue} </p>
+        </div>
+      </div>
+      <div className="mb-4">
+        <div className="bg-blue-100 w-[250px] p-2 rounded-lg inline-block">
+          <p className="text-[15px]"> I understand how upsetting it's to loose your money.
+
+We have raised a ticket for your issue.
+We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
+        </div>
+      </div>
+      {/* More messages can be added here */}
+    
+      <input className="mt-14 border-2 border-black"
+          type="text"
+          value={inputValue}  
+          onChange={handleInputChange} 
+        />
+    </div>
+  );
+};
+
+export default Chatmain;
