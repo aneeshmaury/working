9fa921da9b2e1902a1dedb1d663ea456d22a0d30
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-11 09:59:42 +0530

    khhgkg

--- app/chat1.js
+++ app/chat1.js
@@ -11,7 +11,7 @@ const Chatmain = () => {
   return (
     <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
       <div className="mb-4">
-        <div className="bg-slate-100 w-[270px] p-2 pr-4 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
+        <div className="bg-slate-50 w-[270px] p-2 pr-4 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px]"> Please elaborate your issue for faster resolution</p>
         </div>
       </div>
@@ -21,7 +21,7 @@ const Chatmain = () => {
         </div>
       </div>
       <div className="mb-4">
-        <div className="bg-slate-100 w-[270px] p-3 pr-5 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
+        <div className="bg-slate-50 w-[270px] p-3 pr-5 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px]">I understand how upsetting it is to loose your money.We have raised a ticket for your issue.We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
         </div>
       </div>
