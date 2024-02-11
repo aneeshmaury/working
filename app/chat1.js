6272efba98b50cb9174c22e2f76860f433f72c68
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-11 09:05:29 +0530

    wqeqwe

--- app/chat1.js
+++ app/chat1.js
@@ -12,27 +12,29 @@ const Chatmain = () => {
     <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
       <div className="mb-4">
         <div className="bg-blue-50 w-[270px] p-2 pr-4 rounded-lg inline-block">
-          <p className="text-[17px]"> Please elaborate your issue for faster resolution</p>
+          <p className="text-[14px]"> Please elaborate your issue for faster resolution</p>
         </div>
       </div>
       <div className="mb-4 text-right">
         <div className="bg-blue-50 p-3 rounded-lg inline-block">
-          <p className="text-[17px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"1px" }} >{inputValue} </p>
+          <p className="text-[14px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"1px" }} >{inputValue} </p>
         </div>
       </div>
       <div className="mb-4">
         <div className="bg-blue-50 w-[270px] p-3 pr-5 rounded-lg inline-block">
-          <p className="text-[17px]">I understand how upsetting it is to loose your money.We have raised a ticket for your issue.We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
+          <p className="text-[14px]">I understand how upsetting it is to loose your money.We have raised a ticket for your issue.We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
         </div>
       </div>
       {/* More messages can be added here */}
-    
+    <div className="bg-red-500np">
       <input className="mt-20 border-2 border-black px-3 py-2 text-black rounded-sm"
           type="text"
           placeholder="Write Your Message..."
           value={inputValue}  
           onChange={handleInputChange} 
         />
+    </div>
+      
     </div>
   );
 };
