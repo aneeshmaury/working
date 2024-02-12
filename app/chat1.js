afab18d5a0e7c5e49331a73ade97d046d701f88c
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-12 08:28:00 +0530

    jfskh

--- app/chat1.js
+++ app/chat1.js
@@ -11,7 +11,7 @@ const Chatmain = () => {
   return (
     <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
       <div className="mb-4">
-      <p className="text-slate-400 text-[14px]">Bank Assistant <span className="text-[12px] ml-2">a few seconds ago</span></p>
+      <p className="text-slate-400 text-[13px]">Bank Assistant <span className="text-[11px] ml-2">a few seconds ago</span></p>
         <div className="bg-[#e6ebeb] w-[270px] p-2 pr-4 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px]"> Please elaborate your issue for faster resolution</p>
         </div>
@@ -20,11 +20,15 @@ const Chatmain = () => {
         <div className="bg-sky-200 p-3 rounded-tl-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"1px" }} >{inputValue} </p>
         </div>
+      <p className="text-slate-400 text-[9px]">Delivered a few seconds ago</p>
+
       </div>
       <div className="mb-4">
         <div className="bg-[#e6ebeb] w-[270px] p-3 pr-5 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px]">I understand how upsetting it is to loose your money.We have raised a ticket for your issue.We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
         </div>
+      <p className="text-slate-400 text-[13px]">Bank Assistant <span className="text-[11px] ml-2">a few seconds ago</span></p>
+
       </div>
       {/* More messages can be added here */}
     <div className="bg-red-500np">
